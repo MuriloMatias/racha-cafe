f5ffd48523ddfd91eb9a03c038a9ad0d6df485fe
Murilo Matias <m.matias@ufms.br>
2023-11-04 00:40:31 -0400

    build: criada base para o projeto

---
+++ assinaturaMicroservice/src/main/java/com/assinaturamicroservice/assinaturaMicroservice/AssinaturaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.assinaturamicroservice.assinaturaMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssinaturaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssinaturaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ assinaturaMicroservice/src/test/java/com/assinaturamicroservice/assinaturaMicroservice/AssinaturaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assinaturamicroservice.assinaturaMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssinaturaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
